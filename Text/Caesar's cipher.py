b90864caada59f01faff33684b8596db64aaa23e
jarik-marwede <jarik.marwede@gmail.com>
2017-11-18 22:12:43 +0100

    Add "Text" category

---
+++ Text/Caesar's cipher.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""Encrypt/Decrypt strings by shifting them in alphabeticall order
+
+Title: Caesar's cipher
+
+Description:
+The user first enters a number to be used as the shift parameter.
+The shift parameter is the key to the cipher and
+would be used for encrypting and decrypting text.
+During encryption, shifting is done to the left of the alphabets
+till the shift parameter matches the number of shifts done.
+For decryption, reverse the above process.
+For example with a shift parameter of 3:
+Plain:
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+Cipher:
+XYZABCDEFGHIJKLMNOPQRSTUVW
+During decryption:
+Plaintext:
+THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG
+Ciphertext:
+QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD
+Submitted by Neo
+"""
+
+
+def encrypt(string: str, shift: int) -> str:
+    """Return encrypted version of string
+    """
+    encrypted_string = ""
+    alphabet = ("a", "b", "c", "d", "e", "f",
+                "g", "h", "i", "j", "k", "l",
+                "m", "n", "o", "p", "q", "r",
+                "s", "t", "u", "v", "w", "x",
+                "y", "z")
+
+    for char in string:
+        if char == char.lower():
+            uppercase = False
+        else:
+            uppercase = True
+
+        if char.lower() in alphabet:
+            char_index = alphabet.index(char.lower())
+            encrypted_char_index = char_index - shift
+            while encrypted_char_index > 25:
+                encrypted_char_index -= 26
+            while encrypted_char_index < 0:
+                encrypted_char_index += 26
+            if uppercase is True:
+                encrypted_string += alphabet[encrypted_char_index].upper()
+            else:
+                encrypted_string += alphabet[encrypted_char_index]
+        else:
+            encrypted_string += char
+    return encrypted_string
+
+
+def decrypt(string: str, shift: int) -> str:
+    """Return decrypted version of string
+    """
+    return encrypt(string, 26 - shift)
+    
+
+if __name__ == "__main__":
+    STRING = input("Please input the string that you want to encrypt: ")
+    SHIFT = int(input("Please input the rotation: "))
+    ENCRYPTED = encrypt(STRING, SHIFT)
+    print("Encrypted: " + ENCRYPTED)
+    print("Decrypted: " + decrypt(ENCRYPTED, SHIFT))
+    
---
+++ Text/Morse Code Maker.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""Convert text to morse code or morse code to text
+
+Title: Morse Code Maker
+
+Description:
+Make a program which takes in a sting from the user
+and then outputs that as a string of Morse Code.
+If you are infamiliar with Morse Code, you can find it on Wikipedia.
+"""
+MORSE_CODE_DICT = {" ": "/", "a": ".-", "b": "-...", "c": "-.-.",
+                   "d": "-..", "e": ".", "f": "..-.", "g": "--.",
+                   "h": "....", "i": "..", "j": ".---", "k": "-.-",
+                   "l": ".-..", "m": "--", "n": "-.", "o": "---",
+                   "p": ".--.", "q": "--.-", "r": ".-.", "s": "...",
+                   "t": "-", "u": "..-", "v": "...-", "w": ".--",
+                   "x": "-..-", "y": "-.--", "z": "--..", "0": "-----",
+                   "1": ".----", "2": "..---", "3": "...--", "4": "....-",
+                   "5": ".....", "6": "-....", "7": "--...", "8": "---..",
+                   "9": "----.", ".": ".-.-.-", ",": "--..--", "?": "..--..",
+                   "'": ".----.", "!": "-.-.--", "/": "-..-.", "(": "-.--.",
+                   ")": "-.--.-", "&": ".-...", ":": "---...", ";": "-.-.-.",
+                   "=": "-...-", "+": ".-.-.", "-": "-....-", "_": "..--.-",
+                   "\"": ".-..-.", "$": "...-..-", "@": ".--.-.", "à": ".--.-",
+                   "ä": ".-.-", "å": ".--.-", "ą": ".-.-", "æ": ".-.-",
+                   "ć": "-.-..", "ĉ": "-.-..", "ç": "-.-..", "đ": "..-..",
+                   "ð": "..--.", "é": "..-..", "è": ".-..-", "ę": "..-..",
+                   "ĝ": "--.-.", "ĥ": "----", "ĵ": ".---.", "ł": ".-..-",
+                   "ń": "--.--", "ñ": "--.--", "ó": "---.", "ö": "---.",
+                   "ø": "---.", "ś": "...-...", "ŝ": "...-.", "š": "----",
+                   "þ": ".--..", "ü": "..--", "ŭ": "..--", "ź": "--..-.",
+                   "ż": "--..-"}
+
+
+def convert_to_morse_code(string: str) -> str:
+    """Convert to morse code
+    """
+    new_string = ""
+    for char in string:
+        if char.lower() in MORSE_CODE_DICT:
+            new_string += MORSE_CODE_DICT[char.lower()] + " "
+        else:
+            raise ValueError("Invalid character: {}".format(char))
+    new_string = new_string[:len(new_string) - 1]
+    return new_string
+
+
+def convert_from_morse_code(string: str) -> str:
+    """Convert from morse code
+    """
+    new_string = ""
+    for letter_code in string.split():
+        for char, morse_code in MORSE_CODE_DICT.items():
+            if letter_code == morse_code:
+                new_string += char
+    return new_string
+
+
+if __name__ == "__main__":
+    STRING = input("Please input a string: ")
+    MORSE_CODE = convert_to_morse_code(STRING)
+    print(MORSE_CODE)
+    print(convert_from_morse_code(MORSE_CODE))
