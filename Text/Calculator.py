98acf2886280ab9091fc5a5dad6663522979f13d
jarik-marwede <33765260+jarik-marwede@users.noreply.github.com>
2017-11-20 16:12:00 +0100

    Added Calculator to Text Category
    Calculator uses eval() to calculate expressions. A better version that does not use eval should be made.

---
+++ Text/Calculator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Calculates a mathematical expression
+
+Title:
+Calculator
+
+Description:
+Your program will accept a mathematical expression as a string,
+parse it and output the answer of the expression.
+For example, "2*3+2-(4+5)" will output 9.
+For added complexity,
+add the ability to calculate expressions with scientifix terms
+like cos, sin, tan, log, mod, dqr etc.
+Submitted by David
+"""
+
+
+def evaluate(expression: str) -> float:
+    """Returns the value of expression using eval()
+    """
+    try:
+        result = eval(expression)
+        return result
+    except:
+        raise ValueError(expression + " is not a mathematical expression.")
+
+
+if __name__ == "__main__":
+    while True:
+        EXPRESSION = input("Please input a mathematical expresion: ")
+        print(evaluate(EXPRESSION))
