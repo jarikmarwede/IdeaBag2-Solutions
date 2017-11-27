7839169d619bb5610cfd9a801c34c9c8a8f612b0
jarik-marwede <33765260+jarik-marwede@users.noreply.github.com>
2017-11-27 19:37:08 +0100

    Added Lottot to Numbers category

---
+++ Numbers/Lotto.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Title:
+Lotto
+
+Description:
+Create a program which asks for 6 numbers in the range 1-49.
+The program should then display 6 random numbers form the same range
+and check how many numbers the user guessed correctly.
+For added complexity, make the program into a mini-game.
+Allow the user to retry the lotto, show their payout after each round,
+their highest payout after each round, their hoghest payout etc.
+Feel free to add as much complexity as you want.
+Submitted by Filipekczek7
+"""
+import random
+
+
+def gamble() -> list:
+    result = random.choices(range(1, 50), k=6)
+    return result
+
+
+def lotto(guesses: list) -> int:
+    result = gamble()
+    right = 0
+    for guess in guesses:
+        if guess in result:
+            right += 1
+    return right
+
+
+if __name__ == "__main__":
+    while True:
+        RAW_GUESSES = input("Please input your guesses "
+                            "separated by commas: ")
+        GUESSES = []
+        for GUESS in RAW_GUESSES.split(","):
+            GUESSES.append(int(GUESS))
+        print(lotto(GUESSES))
