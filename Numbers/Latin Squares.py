c71c4cf812eb168e7be64a84c605de7bae56b627
jarik-marwede <33765260+jarik-marwede@users.noreply.github.com>
2017-11-28 20:11:36 +0100

    Added Module Docstring

--- Numbers/Latin Squares.py
+++ Numbers/Latin Squares.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
+"""
+Title:
+Latin Squares
+
+Description:
+A Latin square is an n x n array filled with n different symbols 
+each occuring exactly once in each row and exactly once in each column.
+For example
+1
+And,
+1 2
+2 1
+are both valid Latin squares.
+For this program you have to check whether a given array is a Latin square.
+If it is a Latin square, then display true otherwise display false.
+The user will provide two inputs:
+1. n - The length of a row in the array
+2. A string representing the array of symbols
+Example input
+5
+1234551234451233451223451
+2
+1334
+4
+1234132423414321
+Example output
+true
+false
+false
 
 
 def is_latin_square(row_length: int, array: str) -> bool:
