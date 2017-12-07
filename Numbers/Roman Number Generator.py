6f293c7fa30779d1cddb43c20a8f326b1000fad3
jarik-marwede <33765260+jarik-marwede@users.noreply.github.com>
2017-12-07 21:06:52 +0100

    Added Roman Number Generator to Numbers category

---
+++ Numbers/Roman Number Generator.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+"""
+Title:
+Roman Number Generator
+
+Description:
+Develop a program that accepts an integer
+and outputs the Roman Number equivalent of that number.
+Examples:
+4 - IV
+12 - XII
+20 - XX
+Submitted by SoReNa
+"""
+
+
+def arabic_to_roman(number: int) -> str:
+    """Return roman version of the specified arabic number
+    """
+    if number > 3999:
+        raise ValueError("Number can not be represented with roman numerals: "
+                         "{}".format(number))
+    roman_number = ""
+    for index, digit in enumerate(reversed(str(number))):
+        if index == 0:
+            first = "I"
+            fifth = "V"
+            tenth = "X"
+        elif index == 1:
+            first = "X"
+            fifth = "L"
+            tenth = "C"
+        elif index == 2:
+            first = "C"
+            fifth = "D"
+            tenth = "M"
+        elif index == 3:
+            first = "M"
+            fifth = ""
+            tenth = ""
+        else:
+            raise ValueError("Invalid input: {}".format(number))
+
+        if digit == "0":
+            continue
+        elif digit == "1":
+            roman_number = first + roman_number
+        elif digit == "2":
+            roman_number = first * 2 + roman_number
+        elif digit == "3":
+            roman_number = first * 3 + roman_number
+        elif digit == "4":
+            roman_number = first + fifth + roman_number
+        elif digit == "5":
+            roman_number = fifth + roman_number
+        elif digit == "6":
+            roman_number = fifth + first + roman_number
+        elif digit == "7":
+            roman_number = fifth + first * 2 + roman_number
+        elif digit == "8":
+            roman_number = fifth + first * 3 + roman_number
+        elif digit == "9":
+            roman_number = first + tenth + roman_number
+    return roman_number
+
+
+if __name__ == "__main__":
+    while True:
+        NUMBER = int(input("Please type in the number "
+                           "you want to convert: "))
+        print(arabic_to_roman(NUMBER))
+        print("")
