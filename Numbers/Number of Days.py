8bbd92ff672780f7b543df1afda8f5acb7ce3b42
jarik-marwede <33765260+jarik-marwede@users.noreply.github.com>
2018-03-27 14:58:33 +0200

    First commit to Number of Days

---
+++ Numbers/Number of Days.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+
+Title:
+Number of Days
+
+Description:
+Your program should take two string inputs from the user
+in the format (dd/mm/yyyy)
+and calculate the number of days between those two dates.
+Submitted by Kanishk
+"""
+
+
+def number_of_days(date1: str, date2: str) -> int:
+    """Return the number of days between two dates."""
+    pass
+
+
+def _start():
+    """Start program interactively."""
+    pass
+
+
+if __name__ == "__main__":
+    _start()
