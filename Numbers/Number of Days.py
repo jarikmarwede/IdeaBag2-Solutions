a5e486f4f9f4bd302b0f75062ac5a7cc34ef93ff
jarik-marwede <33765260+jarik-marwede@users.noreply.github.com>
2018-03-27 16:15:12 +0200

    Added first version of Number of Days to Numbers category

--- Numbers/Number of Days.py
+++ Numbers/Number of Days.py
@@ -14,12 +14,82 @@ Submitted by Kanishk
 
 def number_of_days(date1: str, date2: str) -> int:
     """Return the number of days between two dates."""
-    pass
+    if date1 == date2:
+        return 0
+    days_between = 0
+    month_dictionary = {1: 31,
+                        2: 28,
+                        3: 31,
+                        4: 30,
+                        5: 31,
+                        6: 30,
+                        7: 31,
+                        8: 31,
+                        9: 30,
+                        10: 31,
+                        11: 30,
+                        12: 31}
+    date1_split = date1.split("/")
+    date2_split = date2.split("/")
+    day1 = int(date1_split[0])
+    day2 = int(date2_split[0])
+    month1 = int(date1_split[1])
+    month2 = int(date2_split[1])
+    year1 = int(date1_split[2])
+    year2 = int(date2_split[2])
+
+    if year1 > year2:
+        days_between += 365 * (year1 - year2)
+        if month1 > month2:
+            months_between = [month for month in range(0, 13)
+                              if month1 > month >= month2]
+            days_between += sum([month_dictionary[month]
+                                 for month in months_between])
+        elif month2 > month1:
+            months_between = [month for month in range(0, 13)
+                              if month2 > month >= month1]
+            days_between -= sum([month_dictionary[month]
+                                 for month in months_between])
+        days_between += day1 - day2
+    elif year2 > year1:
+        days_between += 365 * (year2 - year1)
+        if month2 > month1:
+            months_between = [month for month in range(0, 13)
+                              if month2 > month >= month1]
+            days_between += sum([month_dictionary[month]
+                                 for month in months_between])
+        elif month1 > month2:
+            months_between = [month for month in range(0, 13)
+                              if month1 > month >= month2]
+            days_between -= sum([month_dictionary[month]
+                                 for month in months_between])
+        days_between += day2 - day1
+    else:
+        if month1 > month2:
+            months_between = [month for month in range(0, 13)
+                              if month1 > month >= month2]
+            days_between += sum([month_dictionary[month]
+                                 for month in months_between])
+            days_between += day1 - day2
+        elif month2 > month1:
+            months_between = [month for month in range(0, 13)
+                              if month2 > month >= month1]
+            days_between += sum([month_dictionary[month]
+                                 for month in months_between])
+            days_between += day2 - day1
+        else:
+            days_between += max(day1, day2) - min(day1, day2)
+
+    return days_between
 
 
 def _start():
     """Start program interactively."""
-    pass
+    date1 = input("Please type in the first date: ")
+    date2 = input("Please type in the second date: ")
+
+    print(f"The number of days between {date1} and {date2} "
+          f"is {number_of_days(date1, date2)}.")
 
 
 if __name__ == "__main__":
