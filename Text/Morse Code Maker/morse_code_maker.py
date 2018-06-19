6763583e36d2b57221eba94e2c2c552c3d791da8
Jarik Marwede <33765260+jarikmarwede@users.noreply.github.com>
2018-06-19 09:10:35 +0200

    Cleaned up code

--- Text/Morse Code Maker/morse_code_maker.py
+++ Text/Morse Code Maker/morse_code_maker.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Convert text to morse code or morse code to text
+"""Convert text to morse code or morse code to text.
 
 Title:
 Morse Code Maker
@@ -7,7 +7,7 @@ Morse Code Maker
 Description:
 Make a program which takes in a sting from the user
 and then outputs that as a string of Morse Code.
-If you are infamiliar with Morse Code, you can find it on Wikipedia.
+If you are unfamiliar with Morse Code, you can find it on Wikipedia.
 """
 MORSE_CODE_DICT = {" ": "/", "a": ".-", "b": "-...", "c": "-.-.",
                    "d": "-..", "e": ".", "f": "..-.", "g": "--.",
@@ -34,21 +34,19 @@ MORSE_CODE_DICT = {" ": "/", "a": ".-", "b": "-...", "c": "-.-.",
 
 
 def convert_to_morse_code(string: str) -> str:
-    """Convert to morse code
-    """
+    """Convert to morse code."""
     new_string = ""
     for char in string:
         if char.lower() in MORSE_CODE_DICT:
             new_string += MORSE_CODE_DICT[char.lower()] + " "
         else:
-            raise ValueError("Invalid character: {}".format(char))
+            raise ValueError(f"Invalid character: {char}")
     new_string = new_string[:len(new_string) - 1]
     return new_string
 
 
 def convert_from_morse_code(string: str) -> str:
-    """Convert from morse code
-    """
+    """Convert from morse code."""
     new_string = ""
     for letter_code in string.split():
         for char, morse_code in MORSE_CODE_DICT.items():
@@ -57,9 +55,14 @@ def convert_from_morse_code(string: str) -> str:
     return new_string
 
 
-if __name__ == "__main__":
+def _start_interactively():
+    """Start the program interactively through the command line."""
     while True:
-        STRING = input("Please input a string: ")
-        MORSE_CODE = convert_to_morse_code(STRING)
-        print(MORSE_CODE)
-        print(convert_from_morse_code(MORSE_CODE))
+        string = input("Please input a string: ")
+        morse_code = convert_to_morse_code(string)
+        print("Morse Code: " + morse_code)
+        print("Converted back: " + convert_from_morse_code(morse_code) + "\n")
+
+
+if __name__ == "__main__":
+    _start_interactively()
