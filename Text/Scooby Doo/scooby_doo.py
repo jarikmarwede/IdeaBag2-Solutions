ef539bd4eb544af199763d2ac0a985d3a1354c07
Jarik Marwede <33765260+jarikmarwede@users.noreply.github.com>
2018-06-19 08:58:30 +0200

    Cleaned up code

--- Text/Scooby Doo/scooby_doo.py
+++ Text/Scooby Doo/scooby_doo.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""Replace all characters in each word in string with "r"
-until a vowel appears in it
+"""Replace all characters in each word with "r" until a vowel appears in it.
 
 Title:
 Scooby Doo
@@ -15,35 +14,42 @@ For example, the word "scooby" would become "rooby".
 Write a program that "Scooby doos" any word or sentence a user inputs.
 You can choose to make the letter 'Y' either a consonant or a vowel.
 """
+VOWELS = ("a", "e", "i", "o", "u")
+CONSONANTS = ("b", "c", "d", "f", "g",
+              "h", "j", "k", "l", "m",
+              "n", "p", "q", "r", "s",
+              "t", "v", "w", "x", "z")
 
 
 def scoobydoo(string: str) -> str:
-    """Return a scooby dooed version of string
-    """
+    """Return a scooby doo version of string."""
     new_string = ""
-    vowels = ("a", "e", "i", "o", "u")
-    consonants = ("b", "c", "d", "f", "g",
-                  "h", "j", "k", "l", "m",
-                  "n", "p", "q", "r", "s",
-                  "t", "v", "w", "x", "z")
+
     for word in string.split():
         skip = False
-        for index, char in enumerate(word):
+
+        for char in word:
             if skip is True:
                 new_string += char
-            elif char.lower() in vowels:
+            elif char.lower() in VOWELS:
                 new_string += char
                 skip = True
-            elif char.lower() in consonants:
+            elif char.lower() in CONSONANTS:
                 new_string += "r"
             else:
                 new_string += char
         new_string += " "
+
     new_string = new_string.rstrip()
     return new_string
 
 
-if __name__ == "__main__":
+def _start_interactively():
+    """Start the program interactively through the command line."""
     while True:
-        STRING = input("Please input a string: ")
-        print(scoobydoo(STRING))
+        string = input("Please input a string: ")
+        print(scoobydoo(string) + "\n")
+
+
+if __name__ == "__main__":
+    _start_interactively()
