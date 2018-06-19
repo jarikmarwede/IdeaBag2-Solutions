563cf8f1465a1d9cd79cf63f302b26061209f643
Jarik Marwede <33765260+jarikmarwede@users.noreply.github.com>
2018-06-19 09:03:13 +0200

    Cleaned up code

--- Text/Rovarspraket/rovarspraket.py
+++ Text/Rovarspraket/rovarspraket.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Translate to and from rovarspraket
+"""Translate to and from Rovarspraket.
 
 Title:
 Rovarspraket
@@ -10,26 +10,26 @@ is not very complicated.
 You take an ordinary word and replace the consonants
 with the consonant doubled with an 'o' between them.
 For example 'b' becomes 'bob', 'm' becomes 'mom'.
-Voweld are left intact.
-Make a program that translates any lengh of string into Rovarspraket.
+Vowels are left intact.
+Make a program that translates any length of string into Rovarspraket.
 For added difficulty,
 make it able to translate to and from Rovarspraket.
 """
+VOWELS = ("a", "e", "i", "o", "u")
+CONSONANTS = ("b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
+              "n", "p", "q", "r", "s", "t", "v", "x", "z")
 
 
 def translate_to(string: str) -> str:
-    """Return string translated to rovarspraket
-    """
-    vowels = ("a", "e", "i", "o", "u")
-    consonants = ("b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
-                  "n", "p", "q", "r", "s", "t", "v", "x", "z")
+    """Return string translated to Rovarspraket."""
     new_string = ""
+
     for index, char in enumerate(string):
-        if char.lower() in consonants:
+        if char.lower() in CONSONANTS:
             new_string += "".join((char, "o", char))
         elif char == "w":
             if index < 0:
-                if char == "w" and string[index - 1].lower() in vowels:
+                if char == "w" and string[index - 1].lower() in VOWELS:
                     new_string += "w"
                 else:
                     new_string += "wow"
@@ -41,26 +41,23 @@ def translate_to(string: str) -> str:
 
 
 def translate_from(string: str) -> str:
-    """Return string translated from rovarspraket
-    """
-    vowels = ("a", "e", "i", "o", "u")
-    consonants = ("b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
-                  "n", "p", "q", "r", "s", "t", "v", "x", "z")
+    """Return string translated from Rovarspraket."""
     new_string = ""
     skip = 0
+
     for index, char in enumerate(string):
         if skip > 0:
             skip -= 1
             continue
         if index < len(string) - 2:
-            if (char.lower() in consonants and
+            if (char.lower() in CONSONANTS and
                     string[index + 1] == "o" and
                     string[index + 2] == char):
                 new_string += char
                 skip = 2
             elif index != 0:
                 if (char.lower() == "w" and
-                        string[index - 1].lower() not in vowels and
+                        string[index - 1].lower() not in VOWELS and
                         string[index + 1] == "o" and
                         string[index + 2] == char):
                     new_string += char
@@ -74,9 +71,14 @@ def translate_from(string: str) -> str:
     return new_string
 
 
-if __name__ == "__main__":
+def _start_interactively():
+    """Start the program interactively through the command line."""
     while True:
-        STRING = input("Please input a string: ")
-        ROVARSPRAKET = translate_to(STRING)
-        print(ROVARSPRAKET)
-        print(translate_from(ROVARSPRAKET))
+        string = input("Please input a string: ")
+        rovarspraket = translate_to(string)
+        print("Rovarspraket:", rovarspraket)
+        print("Translated back:", translate_from(rovarspraket) + "\n")
+
+
+if __name__ == "__main__":
+    _start_interactively()
