d9c90d8705f3ff79cd35cf9fae6bc01ad438e28c
Jarik Marwede <jarikmarwede@zoho.eu>
2018-09-16 18:33:28 +0200

    Added unittests for Roman Number Generator

---
+++ Numbers/Roman Number Generator/tests.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import unittest
+
+from roman_number_generator import arabic_to_roman
+
+
+class Test(unittest.TestCase):
+
+    def _start_arabic_to_roman(self):
+        self.assertRaises(ValueError, arabic_to_roman, 4000)
+        self.assertEqual(arabic_to_roman(4), "IV")
+        self.assertEqual(arabic_to_roman(12), "XII")
+        self.assertEqual(arabic_to_roman(20), "XX")
+
+
+if __name__ == "__main__":
+    unittest.main()
