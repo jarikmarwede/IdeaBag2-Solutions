f1ea40d8340302ea141e763be62e193f11d0ce1c
Jarik Marwede <jarikmarwede@zoho.eu>
2018-09-20 16:56:47 +0200

    Added unittests for Scooby Doo

---
+++ Text/Scooby Doo/tests.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import unittest
+
+from scooby_doo import scoobydoo
+
+
+class Test(unittest.TestCase):
+
+    def test_scoobydoo(self):
+        self.assertEqual(scoobydoo("scooby"), "rrooby")
+
+
+if __name__ == "__main__":
+    unittest.main()
